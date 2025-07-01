510019a8498a88292fa0fdda0e9900e5e5d090c2
Harivar <109642763+Harivar@users.noreply.github.com>
2025-07-01 14:50:35 +0530

    Login/register

---
+++ 01.07.2025/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodappMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
---
+++ 01.07.2025/DTO/LoginDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodappMVC.DTO
+{
+    public class LoginDTO
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+        [Required(ErrorMessage = "Captcha is required")]
+        public string CaptchaText { get; set; }
+
+        public string? CaptchaI { get; set; }
+
+        //[Range(typeof(bool), "true", "true", ErrorMessage = "Please confirm you are not a robot")]
+        public bool NotRobot { get; set; }
+    }
+}
