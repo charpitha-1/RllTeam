446e33da6bc95a8fd6e2d201fe5017c91597b067
charpitha-1 <thavishala279@gmail.com>
2025-07-03 11:20:08 +0530

    OrderDetails Controller

---
+++ OrderDetailsController/FoodItemDTO.cs
@@ -0,0 +1,16 @@
+﻿using FoodieApp.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoodieApp.DTO
+{
+    public class FoodItemDTO
+    {
+        public int FoodItemId { get; set; }  // Required for PUT/DELETE
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; } = 50;
+
+        public int RestaurantId { get; set; } // Foreign key to restaurant
+    }
+}
+
---
+++ OrderDetailsController/OrderDetailsDTO.cs
@@ -0,0 +1,30 @@
+﻿namespace FoodieApp.DTO
+{
+
+    public class OrderDetailsDTO
+    {
+        
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public double OrderTotal { get; set; }
+        public double Discount { get; set; }
+        public double Gst { get; set; }
+        public double FinalAmount { get; set; }
+        public string OrderStatus { get; set; }
+        public string DeliveredBy { get; set; }
+        public int UserId { get; set; }
+        public int AddressId { get; set; }
+        public int RestId { get; set; }
+        public DateTime? ScheduleDeliveryAt { get; set; }
+
+        // List of related order line items
+        public List<OrderLineItemDTO> OrderLineItems { get; set; } = new List<OrderLineItemDTO>();
+    }
+    public class OrderLineItemDTO
+    {
+        public int FoodItemId { get; set; }
+        public int Qty { get; set; }
+        public double UnitPrice { get; set; }
+    }
+
+}
